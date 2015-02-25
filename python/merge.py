ebf0a46402416e1ea2f778dca9c46eb1ab7a2c44
Jeffrey Lipton <jeffreyilipton@gmail.com>
2015-02-25 11:24:58 -0500

    alligned tray and heads

--- python/merge.py
+++ python/merge.py
@@ -329,8 +329,8 @@ def mergeFromXML(infilename, outfilename, verbose, debug):
     fabTree = ElementTree(file = infilename)
     for el in fabTree.iter(): el.tag = el.tag.lower()
     
-    BUILDTRAY_OFFSET = [24,32,20]
-    TOOLHEAD_OFFSET  = [63,10,-4.5]
+    BUILDTRAY_OFFSET = [20,32,20]
+    TOOLHEAD_OFFSET  = [-10,63,-4.5]
     
     if debug:
         BUILDTRAY_OFFSET = [0,0,0]
